945636dfd02d5698a5b2d8dec14a3e094a81e7f7
Aman Prasad <aprasad2707@gmail.com>
2023-02-19 20:11:04 +0530

    Adding files

--- app/src/main/java/com/example/databaseuiux/MainActivity.kt
+++ app/src/main/java/com/example/databaseuiux/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.databaseuiux
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -13,7 +15,7 @@ import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var database:  DatabaseReference
+    private lateinit var database:  DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,9 +40,15 @@ class MainActivity : AppCompatActivity() {
                 etEmail.text?.clear()
                 etPassword.text?.clear()
                 Toast.makeText(this,"User Registered Successfully..",Toast.LENGTH_SHORT).show()
-            }// .addOnSuccessListener {
-                // Toast.makeText(this,"Failed!",Toast.LENGTH_SHORT).show()
-            // }
+            }.addOnFailureListener {
+                Toast.makeText(this,"Failed!",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val signinText = findViewById<TextView>(R.id.tvSignin)
+        signinText.setOnClickListener{
+            val openSigninActivity = Intent(this,SigninActivity:: class.java)
+            startActivity(openSigninActivity)
         }
     }
 }
